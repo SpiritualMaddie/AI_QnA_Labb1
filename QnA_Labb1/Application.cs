ff5b547a7d78eb29f0d0f6c52e62971141a0b249
Madde <105611442+SpiritualMaddie@users.noreply.github.com>
2023-08-28 01:02:01 +0200

    Add project files.

---
+++ QnA_Labb1/Application.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QnA_Labb1
+{
+    internal class Application
+    {
+
+        public void WriteIntro()
+        {
+            Console.WriteLine(@"
+This is an app where you can ask questions about 
+spirituality, tarot cards or meditation.
+");
+        }
+
+        public void WriteLogo()
+        {
+            Console.WriteLine(@"
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                SoulGuide
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     (
+     )\
+     {_}
+    .-;-.
+   |'-=-'|
+   |     |
+   |     |
+   |     |
+   |     |
+   '.___.'
+                        ");
+
+        }
+
+        public void EndApp()
+        {
+                Console.Clear();
+                Console.WriteLine(@"
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+You have chosen to quit the app.
+The app will close...");
+                Thread.Sleep(2500);
+                Environment.Exit(0);
+        }
+
+        public static string WrapText(string text, int maxWidth)
+        {
+            var lines = new List<string>();
+
+            // Split the input text into individual words
+            var words = text.Split(' ');
+            var currentLine = "";
+
+            // Iterate over each word in the text
+            foreach (var word in words)
+            {
+                if ((currentLine + word).Length > maxWidth)
+                {
+                    // Trim any extra spaces and add the current line to the list of lines
+                    lines.Add(currentLine.Trim());
+
+                    // Reset the currentLine string to start constructing a new line
+                    currentLine = "";
+                }
+
+                // Add the word to the current line with a space before it
+                currentLine += $" {word}";
+            }
+
+            // After processing all words, if there's any content left in the currentLine, 
+            // add it to the list of lines after trimming extra spaces
+            if (!string.IsNullOrWhiteSpace(currentLine))
+                lines.Add(currentLine.Trim());
+
+            // Join the list of lines using a new line as the separator and return the result
+            return string.Join(Environment.NewLine, lines);
+        }
+
+    }
+}
---
+++ QnA_Labb1/Program.cs
@@ -0,0 +1,119 @@
+﻿global using Azure;
+global using System.Text;
+global using Azure.AI.TextAnalytics;
+global using Microsoft.Extensions.Configuration;
+global using System.Drawing;
+global using Console = Colorful.Console;
+global using Azure.AI.Language.QuestionAnswering;
+
+namespace QnA_Labb1
+{
+    internal class Program
+    {
+        private static TextAnalyticsClient _textAnalyticsClient;
+        private static QuestionAnsweringClient _qnaClient;
+        private static QuestionAnsweringProject _qnaProject;
+        static void Main(string[] args)
+        {
+            // Get config settings from AppSettings
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // Get config values
+            string? cogSvcEndpoint = config["CognitiveServicesEndpoint"];
+            string? cogSvcKey = config["CognitiveServiceKey"];
+            string? qnAKey = config["QnAKey"];
+            string? languageEndpoint = config["RuntimeEndpoint"];
+            string? projectName = config["ProjectName"];
+            string? deploymentName = config["DeploymentName"];
+
+            // Initialize clients
+            _textAnalyticsClient = new TextAnalyticsClient(new Uri(cogSvcEndpoint), new AzureKeyCredential(cogSvcKey));
+            _qnaClient = new QuestionAnsweringClient(new Uri(languageEndpoint), new AzureKeyCredential(qnAKey));
+            _qnaProject = new QuestionAnsweringProject(projectName, deploymentName);
+
+            // Initialize class
+            Application app = new Application();
+
+            // Set console title and color
+            Console.Title = "SoulGuide";
+            Console.ForegroundColor = Color.Coral;
+
+            // ******************** Chatting app starts here **************************
+            try
+            {
+                app.WriteIntro();
+                app.WriteLogo();
+
+                // *****************************Take user input*****************************
+                // Get user input (until they enter "quit")
+                string userText = "";
+                while (userText.ToLower() != "quit")
+                {
+                    // Set encoding to unicode
+                    Console.InputEncoding = Encoding.Unicode;
+                    Console.OutputEncoding = Encoding.Unicode;
+                   
+                    Console.WriteLine("\nPlease ask a question \n('quit' to stop)");
+                    userText = Console.ReadLine();
+
+                    if (userText.ToLower() != "quit")
+                    {
+                        // Call function to ask Custom Question Answering
+                        Console.WriteLine("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        string response = AskQuestion(userText);
+                        Console.WriteLine(Application.WrapText(response, 47));
+
+                        // Call function to detect language
+                        Console.WriteLine("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        string language = GetLanguage(userText);
+                        Console.WriteLine("Language: " + language);
+
+                        // Call function to detect sentiment
+                        string sentiment = GetSentiment(userText);
+                        Console.WriteLine("Sentiment: " + sentiment);
+                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    }
+                }
+                app.EndApp();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static string AskQuestion(string userText)
+        {
+            // Query the QnA bot
+            Response<AnswersResult> response = _qnaClient.GetAnswers(userText, _qnaProject);
+
+            // If there's an answer & the confidence score is above 0.70 return it; otherwise, provide a default response.
+            if (response.Value.Answers.Count > 0 && response.Value.Answers[0].Confidence > 0.70)
+            {
+                return response.Value.Answers[0].Answer;
+            }
+            else
+            {
+                return "I'm sorry, I don't have an answer to that.";
+            }
+        }
+
+        static string GetLanguage(string text)
+        {
+            // Call the service to get the detected language
+            DetectedLanguage detectedLanguage = _textAnalyticsClient.DetectLanguage(text);
+            return detectedLanguage.Name;
+
+        }
+
+        static string GetSentiment(string text)
+        {
+            // Call the service to get the detected sentiment
+            DocumentSentiment sentimentAnalysis = _textAnalyticsClient.AnalyzeSentiment(text);
+            return sentimentAnalysis.Sentiment.ToString();
+        }
+    }
+}
